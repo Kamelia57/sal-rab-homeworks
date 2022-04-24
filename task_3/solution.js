d46da0e2b7b563e3de39c89abce2127ae0080cee
Татьяна <kamelia@MBP-Tatana.lan>
2022-04-24 15:20:38 +0400

    done

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,29 +2,45 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-product = {id:" ",title:" ",count:" ",price:""};
+item = [
+{
+    id:1,
+    title: 'Пицца',
+    count: count+1,
+    price: '500.00'
+},
+{
+    id:2,
+    title: 'Чебурек',
+    count: count+1,
+    price: '120.00'
+}
+]
+console.log (item[1]);
+console.log (item[2]);
+
+
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
-itemCountText = [count]*price;
+itemCountText = { 
+    count: count +1,
+    price: item.price,
+    sum: count * price,
+}
+
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-
-
-    
 let itemCountText = " ";
-
-itemCountText = `${itemCountText + count}x${price} ₽ = ` ;
- 
+itemCountText = itemCountText + count + "x"+ price + "₽ = ";
 let sum = count * price;
 itemCountText = itemCountText + sum;
 itemCountText = itemCountText + "₽ = ";
-console.log (itemCountText);
 
     // 1. Создайте переменную itemCountText, равную пустой строке
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
@@ -38,4 +54,8 @@ console.log (itemCountText);
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
+
+    function newFunction() {
+        console.log(item.title);
+    }
 }
