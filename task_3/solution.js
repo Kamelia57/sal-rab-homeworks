6d99707f8ad556ccc81c7b6c9574d7eb1be6498f
Татьяна <kamelia@MBP-Tatana.lan>
2022-04-26 20:24:04 +0400

    done

--- task_3/solution.js
+++ task_3/solution.js
@@ -28,10 +28,10 @@ console.log (product.price);
 
 
 let itemCountText = "  ";
-itemCountText =  itemCountText + product.count;
-itemCountText =  itemCountText + " × ";
-itemCountText =  itemCountText + product.price;
-itemCountText =  itemCountText + " ₽ = ";
+itemCountText = itemCountText + product.count;
+itemCountText = itemCountText + " × ";
+itemCountText = itemCountText + product.price;
+itemCountText = itemCountText + " ₽ = ";
 let sum = product.count * product.price;
 itemCountText = itemCountText + sum;
 itemCountText = itemCountText + " ₽  ";
