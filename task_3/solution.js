f5f01fe0887c71fce6ac814e8a636656fb8f4f16
Татьяна <kamelia@MBP-Tatana.lan>
2022-04-26 20:22:49 +0400

    massage done

--- task_3/solution.js
+++ task_3/solution.js
@@ -47,6 +47,6 @@ console.log (itemCountText);
 
     // Конец решения задания №3.1.
 
-    return `<div class="cart-item"><div>${product.title}</div><div> ${itemCountText} </div></div>`;
+    return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 
 }
