4176e40cbddf5447e69030a97985d8508000cc70
Татьяна <kamelia@MacBook-Pro-Tatana.local>
2022-05-07 19:28:23 +0400

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,8 +34,9 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    
+    let data = {client:{name,phone},order: {address,sum}, goods: [title,count]};
+    let address = {street,house,entranse,floor,flat};
     let countOfGoods = goods.length;
 
     for (let i = 0; i <= countOfGoods; i += 1) {
@@ -51,3 +52,4 @@ function sendRequest(name, phone, address, goods, sum) {
 
     return jsonData;
 }
+
