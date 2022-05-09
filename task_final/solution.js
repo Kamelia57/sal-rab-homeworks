8d1b12e0e83ead82866a55e313625a81b4ded9bf
Татьяна <kamelia@MacBook-Pro-Tatana.local>
2022-05-09 21:24:02 +0400

    done bitch

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,11 +38,11 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i < countOfGoods; i+=1) {
-        let goods = [{title:" ",count:3}];
-        data.goods = goods.push(goods({ title: 'Пицца', count: 2 }));
+    for (let i = 0; i < countOfGoods; i++) {
+        let goods = [{title:" ",count:" "}];
+        data.goods = goods.push(goods[i].title,goods[i].count);
     }
-    data.order.address = "ул. " + address.street + ", " + "дом "+ address.house + ", " +  address.entrance + " подъезд" + ", " + address.floor+ " этаж" + ", " + "кв " + address.flat;
+    data.order.address = "ул. " + address.street + ", " + "дом "+ address.house + ", " +  address.entrance + " подъезд" + ", " + address.floor+ " этаж" + ", " + " кв " + address.flat;
     data.order.sum = sum;
     data.client = "Иван" + " " + phone;
 
