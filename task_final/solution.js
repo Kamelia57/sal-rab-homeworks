5a9985b0229a63559543ba99a68cbbf7511a25e9
Татьяна <kamelia@MacBook-Pro-Tatana.local>
2022-05-09 20:50:05 +0400

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -40,7 +40,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     for (let i = 0; i < countOfGoods; i+=1) {
         let goods = [{title:" ",count:3}];
-        data.goods = goods.push({title:goods[i].title,count:goods[i].count})
+        data.goods = goods.push(goods[i].title,goods[i].count);
     }
     data.order.address = address;
     data.order.sum = name + phone + address + goods + sum;
