e30c3acea4c6cc3907932525c2126ad76803935c
Татьяна <kamelia@MBP-Tatana.lan>
2022-05-10 00:03:10 +0400

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,7 +38,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i +=1) {
-        ddata.goods[i] = { title: goods[i].title, count: goods[i].count }
+        data.goods[i] = { title: goods[i].title, count: goods[i].count }
     }
     
     data.order.address = "ул. " + address.street + ", " + "дом "+ address.house + ", " +  address.entrance + " подъезд" + ", " + address.floor + " этаж" + ", " + "кв " + address.flat;
