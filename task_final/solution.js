8817d2e0c88f35d1bf3ca2ef7bb159ac8c9413b8
Татьяна <kamelia@MacBook-Pro-Tatana.local>
2022-05-09 21:15:54 +0400

    done

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,7 +42,7 @@ function sendRequest(name, phone, address, goods, sum) {
         let goods = [{title:" ",count:3}];
         data.goods = goods.push(goods[i].title,goods[i].count);
     }
-    data.order.address = "ул. " + address.street + "," + "дом "+ address.house + "," +  address.entrance + " подъезд" + "," + address.floor+ " этаж" + "," + " кв" + address.flat;
+    data.order.address = "ул. " + address.street + ", " + "дом "+ address.house + ", " +  address.entrance + " подъезд" + ", " + address.floor+ " этаж" + ", " + " кв" + address.flat;
     data.order.sum = sum;
     data.client = "Иван" + " " + phone;
 
